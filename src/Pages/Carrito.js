61beb938b39e3ccb4feea3b52e43a70ac15b9c05
Bastibastibasti <ba.herrerah@duocuc.cl>
2025-05-14 16:09:02 -0400

    Estilo

--- src/Pages/Carrito.js
+++ src/Pages/Carrito.js
@@ -1,26 +1,34 @@
 import React from "react";
 
 function Carrito({ carrito, vaciarCarrito }) {
+  // Calcular total
+  const total = carrito.reduce((acc, item) => acc + item.precio, 0);
+
   return (
     <div className="container">
       <h2>Carrito de Compras</h2>
       {carrito.length === 0 ? (
         <p>El carrito está vacío.</p>
       ) : (
-        <>
+        <div className="carrito-box">
           {carrito.map((item, index) => (
-            <div className="card" key={index}>
+            <div className="card item-carrito" key={index}>
               <p>
                 {item.nombre} - ${item.precio}
               </p>
             </div>
           ))}
-          <div style={{ textAlign: "center", marginTop: "30px" }}>
+
+          <div className="carrito-total">
+            <strong>Total:</strong> ${total}
+          </div>
+
+          <div style={{ textAlign: "center", marginTop: "20px" }}>
             <button onClick={vaciarCarrito} className="btn-vaciar">
               Vaciar Carrito
             </button>
           </div>
-        </>
+        </div>
       )}
     </div>
   );
